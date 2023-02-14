4c12af283a2a65d60b952a3872b8450e4118bfdd
Itallo_cardoso <86330435+itallocardoso2@users.noreply.github.com>
2023-02-14 16:43:38 -0300

    Create ex1_10.js

---
+++ cap01/exemplos/js/ex1_10.js
@@ -0,0 +1,6 @@
+var numDias = Number(prompt("Número de dias: "));
+
+var numHoras = Number(prompt("Número de horas: "));
+
+var total = (numDias * 24) + numHoras;
+alert("Nº Dias: " + numDias + "\nN° Horas: " + numHoras + "\nTotal de Horas: " + total);
